8c780f9f5263344e26f69d3f866a4243ad730470
Nicolefonseca98 <37601556+Nicolefonseca98@users.noreply.github.com>
2018-06-12 23:36:58 -0600

    Ventanas interfaz

--- ProyectoDosAlgoritmos/src/proyectodosalgoritmos/ProyectoDosAlgoritmos.java
+++ ProyectoDosAlgoritmos/src/proyectodosalgoritmos/ProyectoDosAlgoritmos.java
@@ -1,6 +1,9 @@
 
 package proyectodosalgoritmos;
 
+import gui.Login;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Nicole Fonseca, Wilmer Mata, Sergio Siles
@@ -8,10 +11,19 @@ package proyectodosalgoritmos;
 public class ProyectoDosAlgoritmos {
 
     /**
+     * Ventana principal login.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        Login login = new Login();
+        JFrame jFrame = new JFrame();
+        jFrame.setUndecorated(true);
+        jFrame.setLocation(450,200);
+        jFrame.setTitle("Login");
+        jFrame.setSize(399, 245);
+        jFrame.add(login);
+        jFrame.setVisible(true);
+        
     }
     
 }
