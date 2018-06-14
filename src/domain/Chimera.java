9614b9e9e48b33b9b7d1288a80218fc0bc024d14
sfss1997 <sergioss1997@gmail.com>
2018-06-13 23:53:51 -0600

    god

--- src/domain/Chimera.java
+++ src/domain/Chimera.java
@@ -19,66 +19,96 @@ public class Chimera {
     private int chimeraRow;
     private int chimeraColumn;
 
-    public Chimera(int chimeraRow, int chimeraColumn) {
+    public Chimera() {
         this.logic = new Logic();
-        this.chimeraRow = chimeraRow;
-        this.chimeraColumn = chimeraColumn;
-        this.logic.cell[chimeraRow][chimeraColumn].setIdAndImage(8);
+        findEarth();
     }
-
-    public void run() {
-        this.logic.cell[chimeraRow][chimeraColumn].setIdAndImage(8);
-        int random;
-        while (true) {
-            random = (int) (Math.random() * 3);
-            if (random == 0) {
-                chimeraUp();
-            } else if (random == 1) {
-                chimeraDown();
-            } else if (random == 2) {
-                chimeraLeft();
-            } else if (random == 3) {
-                chimeraRight();
-            }
-            try {
-                this.wait(3000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Chimera.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-        
-    }
+    
     public void chimeraUp() {
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(1);
-        if (this.chimeraRow > 0 && this.logic.cell[this.chimeraRow - 1][this.chimeraColumn].getID() == 1) {
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(0);
+        if (this.chimeraRow > 0 && this.logic.cell[this.chimeraRow-1][this.chimeraColumn].getID() == 0) {
             this.chimeraRow--;
         }
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(3);
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(7);
     }
 
     public void chimeraDown() {
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(1);
-        if (chimeraRow < this.logic.cell.length - 1 && this.logic.cell[this.chimeraRow + 1][this.chimeraColumn].getID() == 1) {
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(0);
+        if (chimeraRow < this.logic.cell.length-1 && this.logic.cell[this.chimeraRow+1][this.chimeraColumn].getID() == 0) {
             this.chimeraRow++;
         }
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(3);
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(7);
     }
 
     public void chimeraLeft() {
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(1);
-        if (this.chimeraColumn > 0 && this.logic.cell[this.chimeraRow][this.chimeraColumn - 1].getID() == 1) {
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(0);
+        if (this.chimeraColumn > 0 && this.logic.cell[this.chimeraRow][this.chimeraColumn-1].getID() == 0
+                && isEarthLeft() == true) {
             this.chimeraColumn--;
         }
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(3);
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(7);
     }
 
     public void chimeraRight() {
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(1);
-        if (chimeraColumn < this.logic.cell[0].length - 1 && this.logic.cell[this.chimeraRow][this.chimeraColumn + 1].getID() == 1) {
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(0);
+        if (chimeraColumn < this.logic.cell[0].length-1 && this.logic.cell[this.chimeraRow][this.chimeraColumn+1].getID() == 0
+                && isEarthRight() == true) {
             this.chimeraColumn++;
         }
-        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(3);
+        this.logic.cell[this.chimeraRow][this.chimeraColumn].setIdAndImage(7);
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////////////////
+    private boolean isEarthLeft() {
+        if (this.logic.cell[this.chimeraRow + 1][this.chimeraColumn - 1].getID() == 9
+                || this.logic.cell[this.chimeraRow + 1][this.chimeraColumn - 1].getID() == 10) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    private boolean isEarthRight() {
+        if (this.logic.cell[this.chimeraRow + 1][this.chimeraColumn + 1].getID() == 9
+                || this.logic.cell[this.chimeraRow + 1][this.chimeraColumn + 1].getID() == 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEarthDown(int row, int column) {
+        if (row + 1 < this.logic.cell.length) {
+            if ((this.logic.cell[row + 1][column].getID() == 9 || this.logic.cell[row + 1][column].getID() == 10)
+                    && this.logic.cell[row][column].getID() == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    
+
+    private void findEarth() {
+        while (true) {
+            int randomRow = (int) (Math.random() * this.logic.cell.length);
+            int randomColumn = (int) (Math.random() * this.logic.cell[0].length);
+            
+            if (isEarthDown(randomRow, randomColumn) == true) {
+                
+                this.chimeraRow = randomRow;
+                this.chimeraColumn = randomColumn;
+                this.logic.cell[chimeraRow][chimeraColumn].setIdAndImage(7);
+
+                randomRow = this.logic.cell.length;
+                randomColumn = this.logic.cell[0].length;
+                break;
+            }
+        }
+    }
+   
 }
 
